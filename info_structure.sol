2371c8ed664691561a7af43583416f08a8d31101
蔣欣光 Jiang Xin Guang <135217941+ratwitxch@users.noreply.github.com>
2024-10-13 18:34:02 +0800

    personalized
    changed info_structure function and variable names

--- info_structure.sol
+++ info_structure.sol
@@ -1,36 +1,37 @@
 //SPDX-License-Identifier: MIT
-pragma solidity 0.8.23;
+pragma solidity 0.8.26;
 
 contract Info_structure{
     struct Client{
-        string name;
-        uint balance;
+        string player;
+        uint money;
     }
     Client[] clients;
-    function add_clients(string memory _name,
-uint _balance) public returns (uint) {
-Client memory new_client = Client(_name,
-_balance);
+    function add_player(string memory _player,
+uint _money) public returns (uint) {
+Client memory new_client = Client(_player,
+_money);
 clients.push(new_client);
 uint id = clients.length - 1;
 return id;
 }
 
-function get_Client(uint _id) public view returns (string memory){
-    return clients[_id].name;
+function show_player(uint _id) public view returns (string memory){
+    return clients[_id].player;
 }
-function get_balance(uint _id) public view returns (uint) {
-    return clients[_id].balance;
+function show_money(uint _id) public view returns (uint) {
+    return clients[_id].money;
 }
-function update_balance(uint _id, uint _balance) public returns (uint) {
-    clients[_id].balance = _balance;
-    return clients[_id].balance;
+function recount_money(uint _id, uint _money) public returns (uint) {
+    clients[_id].money = _money;
+    return clients[_id].money;
 }
 
-function how_many() public view returns (uint) {
+function count_players() public view returns (uint) {
     return clients.length;
 }
-function remove_Client(uint _id) public{
+
+function kick_player(uint _id) public{
     clients[_id]= clients[clients.length - 1];
     clients.pop();
 }
